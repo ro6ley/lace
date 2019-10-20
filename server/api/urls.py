413537a868ff15c432bbd6251570a9dbbfddb02f
Robley <robleyadrian@gmail.com>
2019-10-20 21:11:11 +0300

    add endpoints for the API

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-]
+from .views import TemplateCreateView, TemplateDetailsView
+
+urlpatterns = {
+    path('templates/', TemplateCreateView.as_view(), name="templates"),
+    path('templates/<int:pk>/', TemplateDetailsView.as_view(), name="template_detail")
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
