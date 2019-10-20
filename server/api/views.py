19e72cb848e9eb7b5879f47cc8b30259116db519
Robley <robleyadrian@gmail.com>
2019-10-20 21:11:21 +0300

    add views for the API

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+from .models import Template
+from .permissions import IsOwner
+from .serializers import TemplateSerializer
+
+
+class TemplateCreateView(generics.ListCreateAPIView):
+    """ View to handle the creation and listing of templates.
+    """
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+    serializer_class = TemplateSerializer
+    queryset = Template.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Save the POST data when creating a new template
+        """
+        # TODO: automatically create slug in case it is not provided
+        serializer.save(owner=self.request.user)
+
+
+class TemplateDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    """
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+    serializer_class = TemplateSerializer
+    queryset = Template.objects.all()
+
