a2162c1bd1a589dfce76d92ffc0d83418675eabd
Robley <robleyadrian@gmail.com>
2019-10-20 21:09:52 +0300

    add template model

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Template(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+    owner = models.ForeignKey('auth.user', related_name="templates",
+                              on_delete=models.CASCADE)
+    is_private = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """ Return a string representation of the model.
+        """
+        return f"{self.title} by {self.owner.username}"
