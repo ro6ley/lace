c276325f15a431d0b35d56efdbeb9fb8aad2ad79
Robley <robleyadrian@gmail.com>
2019-10-20 21:13:07 +0300

    add permissions

---
+++ server/api/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+from .models import Template
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission class to allow only Template owners to edit them.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Return True id permission is granted to the template owner
+        """
+        if isinstance(obj, Template):
+            return obj.owner == request.user
+        return obj.owner == request.user
