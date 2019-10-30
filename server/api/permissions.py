fad7755b7221a906639050109cae09f9688b4602
Robley <robleyadrian@gmail.com>
2019-10-30 19:50:27 +0300

    minor fix

--- server/api/permissions.py
+++ server/api/permissions.py
@@ -4,13 +4,11 @@ from .models import Template
 
 
 class IsOwner(permissions.BasePermission):
-    """
-    Custom permission class to allow only Template owners to edit them.
+    """ Custom permission class to allow only Template owners to edit them.
     """
 
     def has_object_permission(self, request, view, obj):
-        """
-        Return True id permission is granted to the template owner
+        """ Return True id permission is granted to the template owner
         """
         if isinstance(obj, Template):
             return obj.owner == request.user
