1cd3ca62c108b494ec594428fcf638127025a5cd
Robley <robleyadrian@gmail.com>
2020-02-21 11:40:52 +0300

    rename permission to IsOwnerOrReadOnly and give access to safe HTTP methods

--- server/api/permissions.py
+++ server/api/permissions.py
@@ -3,11 +3,16 @@ from rest_framework import permissions
 from .models import Template
 
 
-class IsOwner(permissions.BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
     """ Custom permission class to allow only Template owners to edit them.
     """
 
     def has_object_permission(self, request, view, obj):
-        """ Return True id permission is granted to the template owner
+        """ Return True if permission is granted to the template owner or it's
+        a read only request to a template.
         """
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return obj.owner == request.user
