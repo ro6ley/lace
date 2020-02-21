aa8c7ddb0caa0e7b01f906cc72e4ec37121d828a
Robley <robleyadrian@gmail.com>
2020-02-21 11:48:50 +0300

    switch to ModelViewSet

--- server/api/views.py
+++ server/api/views.py
@@ -1,52 +1,29 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404
-from rest_framework import generics, permissions
-from rest_framework.exceptions import NotFound, PermissionDenied
-from rest_framework.response import Response
+from django.db.models import Q
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Template
-from .permissions import IsOwner
+from .permissions import IsOwnerOrReadOnly
 from .serializers import TemplateSerializer
 
 
-class TemplateCreateView(generics.ListCreateAPIView):
-    """ View to handle the creation and listing of templates.
-    """
-    serializer_class = TemplateSerializer
-    queryset = Template.objects.all()
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
-
-    def perform_create(self, serializer):
-        """ Save the owner data when creating a new template
-        """
-        serializer.save(owner=self.request.user)
-
-    def get_queryset(self):
-        return self.queryset.filter(owner=self.request.user)
-
-
-class TemplateDetailsView(generics.RetrieveUpdateDestroyAPIView):
+class TemplateViewSet(ModelViewSet):
     """ View to handle the viewing and updating of individual templates.
 
     Public templates can be viewed by unauthenticated users.
     """
     serializer_class = TemplateSerializer
     queryset = Template.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwner]
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
 
-    def retrieve(self, request, *args, **kwargs):
-        template = get_object_or_404(self.get_queryset(), pk=self.kwargs["pk"])
-        serializer = TemplateSerializer(template)
-        response = Response(serializer.data)
-
-        user = None
-        if self.request.user.is_authenticated:
-            user = User.objects.get(username=self.request.user.username)
-        
-        if template.is_private and template.owner == user:
-            return response
-        elif not template.is_private:
-            return response
+    def get_queryset(self):
+        user = self.request.user
+        if user.id:
+            return Template.objects.filter(Q(is_private=False) | Q(owner=user))
         else:
-            self.check_object_permissions(self.request, template)
-            return response
+            return Template.objects.filter(is_private=False)
+
+    # Set user as owner of a Template
+    def perform_create(self, serializer):
+        if self.request.user.is_authenticated:
+            serializer.save(owner=self.request.user)
